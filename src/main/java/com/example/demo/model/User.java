f2f8e1d136d130f5c72e03e5abba47e26fd3958f
Artashes Haroyan <art.aro94@gmail.com>
2019-05-31 14:34:49 +0400

    first init

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,23 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import javax.persistence.*;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "USERS")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+
+}
